c20550ebaa1ed56fccb1bf185354d202427910aa
Adam Legradi <adam.legradi@protonmail.ch>
2018-11-29 13:02:12 +0000

    updated the readme and added SAR and self erase to system_resource_report.sh

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # redhat_bash_server_scripts
 Red Hat bash server scritps
+
+Known issues:
+
+system_resource_report.sh
+- when the kernel is the latest installed the: "Latest Kernel:" line returns empty value
+ 
+
--- system_resource_report.sh/system_resource_report.sh
+++ system_resource_report.sh/system_resource_report.sh
@@ -1,51 +1,65 @@
 #!/bin/bash
-# system_report.sh
-# written by: A.L.
+# system_resource_report.sh
+# Simple system resource report generating tool
 
-# Simple system report generating tool
 
 # Global variables come here
 
 
 
 # Script - start
-echo ""
-echo "-- System Rescource Report --"
+echo -e "\n-- System Resource Report --"
 echo "Hostname: `hostname`"
-echo "Public IP: `curl icanhazip.com -s`"
-echo ""
-echo "=========================="
-echo ""
+echo -e "Public IP: `curl icanhazip.com -s`\n"
+
+echo -e "==========================\n"
 echo "-- Hard Disk Space --"
 echo "Free Space"
-echo "`df -h | grep -vE 'tmpfs|boot'`"
-echo ""
+echo -e "`df -h | grep -vE 'tmpfs|boot'`\n"
 echo "Inodes"
-echo "`df -Ti | grep -vE 'tmpfs|boot'`"
-echo ""
-echo "=========================="
-echo ""
+echo -e "`df -Ti | grep -vE 'tmpfs|boot'`\n"
+
+echo -e "==========================\n"
 echo "-- RAM and Swap space --"
-echo "`free -m`"
-echo ""
-echo "=========================="
-echo ""
+echo -e "`free -m`\n"
+
+echo -e "==========================\n"
 echo "-- CPU --"
-echo "`lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('`"
-echo ""
-echo "=========================="
-echo ""
-echo "-- Packages --"
-echo ""
-echo "Last three updates"
+echo -e "`lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('`\n"
+
+echo -e "==========================\n"
+echo -e "-- Packages --\n"
+echo -e "Last three updates\n"
 yum history | head -n 6 | tail -n 5
-echo ""
-echo "Available packages for upgrade: `yum check-update -q | wc -l`"
-echo ""
+echo -e "Available packages for upgrade: `yum check-update -q | wc -l`\n"
 echo "Current Kernel: `uname -r`"
 echo "Latest Kernel:" 
 yum check-update -q | grep -E 'kernel\.' | awk -F " " '{ print $2 }' 
-echo "=========================="
+
+echo -e "==========================\n"
+if [ -e /usr/bin/sar ]; then
+    echo -e "-- Today's server load --\n"
+    echo "`sar -q`"
+else
+    echo "No SAR utility available. Is this intentional?"
+fi
+
+
+# Prompt the user whether it's ok to remove the script or not
+
+echo -e "\n##########################################"
+echo -e "The $0 script has finished running. Self erasing now... is this ok? y/n"
+
+read SELFERASE
+
+if [ $SELFERASE = y ]
+  then
+    rm -f $0
+    echo -e "\nFile: $0 removed!\n"
+  else
+    echo -e "\nThe $0 script has not been removed! Please don't leave me behind unless required later!\n"
+fi
+
 
 # Script - end
 
