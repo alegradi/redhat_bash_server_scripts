bf69c7b8549da6da6d65b16c24ddf5625f5aa98d
Adam Legradi <adam@homespot.fritz.box>
2020-03-11 20:36:58 +0000

    Reworking script to use functions

--- system_resource_report.sh/system_resource_report.sh
+++ system_resource_report.sh/system_resource_report.sh
@@ -1,11 +1,14 @@
 # ###
 # system_resource_report.sh
-# Simple system resource report generating tool for Centos/RHEL servers
+# Simple system resource report generating tool for Centos/RHEL servers - ideal for AM tasks of system check
 # ###
 
-# To Do list
-# Disk health check incorporated?
-#
+# To Do list?:
+# 1, Disk health check incorporated with hdsentinel?
+# 2, Verify if we are running a VM or dedicated server
+# 3, What relevant processes are running
+# 4, Number of sites hosted on the server
+
 
 ### Please don't touch anything below this line if you want the script to function as intended ###
 
@@ -13,10 +16,61 @@
 unset HISTFILE
 
 
+# Function Declaration
+
+function get_basic_info(){
+PUBLIC_IP=$(curl icanhazip.com -s)
+printf "Hostname: $HOSTNAME\n"
+printf "Primary external IP: $PUBLIC_IP\n"
+}
+
+function get_cpu_info(){
+CPU=$(cat /proc/cpuinfo | grep -m 1 'model name' | cut -f2 -d : | awk '$1=$1')
+CPU_NUM=$(lscpu | grep -m 1 '^Socket' | cut -f2 -d : | awk '$1=$1')
+CPU_CORES=$(cat /proc/cpuinfo | grep -m 1 'cpu cores' | cut -f2 -d : | awk '$1=$1')
+CPU_CLOCK=$(cat /proc/cpuinfo | grep -m 1 'cpu MHz' | cut -f2 -d : | awk '$1=$1') 
+printf "CPU: $CPU @ $CPU_CLOCK Mhz\n"
+printf "Physical CPU(s): $CPU_NUM\n"
+printf "CPU Cores: $CPU_CORES\n"
+}
+
+function get_mem_info(){
+RAM=$(free -h | grep -i mem: | cut -f2 -d: | awk '$1=$1' | cut -f1 -d' ')
+printf "RAM: $RAM\n"
+} 
+
+function get_cpu_load(){
+printf "Today's CPU load averages \ *5 Min \ *10 Min \ *15 Min\n"
+sar -q
+}
+
+
 # Script - start
-echo -e "\n-- System Resource Report --"
-echo "Hostname: `hostname`"
-echo -e "Public IP: `curl icanhazip.com -s`\n"
+
+echo -e ""
+echo -e "System Resource Report"
+echo -e "==========================\n"
+echo -e "-- Basic Information --"
+
+get_basic_info
+
+echo -e "==========================\n"
+echo -e "-- Hardware Information --"
+
+get_cpu_info
+get_mem_info
+
+echo -e "==========================\n"
+echo -e "-- Server usage --"
+
+if [ -e /usr/bin/sar ]; then
+    get_cpu_load  
+else
+    echo "No SAR utility available. SAR can be installed with 'yum install sysstat' from base repo"
+fi
+
+
+
 
 echo -e "==========================\n"
 echo "-- Hard Disk Space --"
@@ -24,14 +78,6 @@ echo "Free Space"
 echo -e "`df -h | grep -vE 'tmpfs|boot'`\n"
 echo "Inodes"
 echo -e "`df -Ti | grep -vE 'tmpfs|boot'`\n"
-
-echo -e "==========================\n"
-echo "-- RAM and Swap space --"
-echo -e "`free -m`\n"
-
-echo -e "==========================\n"
-echo "-- CPU --"
-echo -e "`lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('`\n"
 
 echo -e "==========================\n"
 echo -e "-- Packages --\n"
